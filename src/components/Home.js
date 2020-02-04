7a4737ce844e36b8bd57219eb8e0cadadaa2ba72
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-04 20:00:39 +0200

    remove the buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,6 @@ function Home() {
     return(
         <div className="home-wrapper">
             <img className="home-image"src="https://images.pexels.com/photos/618491/pexels-photo-618491.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=400&w=500" alt=""/>
-            <Link to="/productList">
-                <button className="md-button shop-button">Shop</button>
-            </Link>
-            <Link to="/home">
-                <button className="md-button home-button">Home</button>
-            </Link>
         </div>
     );
 }
