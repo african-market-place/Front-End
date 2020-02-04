96cf2cb7cf002fc3e7f83e69cc3f173156fffcfe
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-04 11:34:02 +0200

    add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import ProductList from './components/ProductList';
+
+import Card from './components/Card';
+
 
 import './App.css';
 
@@ -8,7 +16,11 @@ function App(){
     <Router>
       <Switch>
       <header className="App-header">
-       
+        <NavBar />
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route path="/productList" component={ProductList}/>
+        <Route path="/card" component={Card}/> 
       </header>
       </Switch>
     </Router>
@@ -18,6 +30,5 @@ function App(){
 
 }
 export default App;
-
 
 
