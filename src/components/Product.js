b34fdfa26c549f351cf9e3980b9cf6f5530ddba2
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-05 21:59:34 +0200

    adding animation to product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+
+
 import styled from "styled-components";
 import {keyframes} from "styled-components";
 
@@ -20,17 +22,38 @@ display: flex;
 justify-content:center;
 flex-direction: column;
 align-items: center;
-margin: 0 auto;
+margin: 5% auto;
 color: ${props=>props.inputColor || "#f2fcfe"};
 background:#bdc3c7;
-width: 950px;
-height: 400px;
+height: 450px;
+position: relative;
+padding: 25px;
 box-sizing: border-box;
 border: 5px white solid;
-/* border-radius: 50%; */
+border-radius: 4px;
 overflow: hidden;
+max-width: 700px;
 box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6);
-
+animation: fadeInUp 0.6s forwards;
+cursor: pointer;
+&:hover {
+      background: #79868f; 
+     text-decoration: none;
+}
+@keyframes fadeInUp {
+  0% {
+    transform: translateY(100px);
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.product:nth-child(2) {
+  animation-delay: 1s;
+}
+.product:nth-child(3) {
+  animation-delay: 0.4s;
+}
 
 
 `;
