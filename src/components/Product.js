ea82ef611d4e526c0550404bdc6fc916913e9f9d
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-02-06 16:40:26 +0200

    adding icon location

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,40 +1,42 @@
-import React from "react";
+import React from 'react';
+import img8 from '../img/img8/compass-32.png';
 import styled from "styled-components";
 
-export default function Item(props) {
-  return (
-    <Product>
-      <h1>{props.product.name}</h1>
-      <p className="pill">{props.product.price}</p>
-      <p>Location:{props.product.location}</p>
-      <p>Category:{props.product.category}</p>
-    </Product>
-  );
+export default function Item(props){
+    return(
+        <Product>
+            <h1>{props.product.name}</h1>
+            <p className="pill"> ${props.product.price}</p>
+            <p className="place"><img src={img8} alt="Home" className="navbar-img1"></img>{props.product.location}</p>
+            <p className="group">Category:{props.product.category}</p>
+        </Product>
+    );
 }
 
 const Product = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  margin: 5%;
-  width: 20%;
-  color: ${props => props.inputColor || "#f2fcfe"};
-  background: #bdc3c7;
-  height: 450px;
-  position: relative;
-  padding: 25px;
-  box-sizing: border-box;
-  border: 5px white solid;
-  border-radius: 4px;
-  overflow: hidden;
-  box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
-  animation: fadeInUp 0.6s forwards;
-  cursor: pointer;
-  &:hover {
-    background: #79868f;
-    text-decoration: none;
-  }
+display: flex;
+justify-content:center;
+flex-direction: column;
+align-items: center;
+margin: 5%;
+width: 20%;
+color: ${props=>props.inputColor || "#f2fcfe"};
+/* background:#bdc3c7; */
+height: 450px;
+position: relative;
+padding: 25px;
+box-sizing: border-box;
+border: 5px white solid;
+border-radius: 4px;
+overflow: hidden;
+box-shadow: 0 5px 15px 0px rgba(0,0,0,0.6);
+animation: fadeInUp 0.6s forwards;
+cursor: pointer;
+&:hover {
+      background: #79868f; 
+     text-decoration: none;
+}
+
   @keyframes fadeInUp {
     0% {
       transform: translateY(100px);
